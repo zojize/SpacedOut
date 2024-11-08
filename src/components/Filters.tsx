fe666a197dc52cec3790a9ce511e6c265ea53344
zojize <jizezou2@illinois.edu>
2024-11-08 17:15:47 -0600

    Cleanup filter trigger button

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -2,14 +2,21 @@
 
 import { Filter } from 'lucide-react';
 import { Drawer } from 'vaul';
+import { Button } from './ui/button';
 
 export function Filters() {
   return (
     // https://vaul.emilkowal.ski/
     <Drawer.Root direction="right">
-      <Drawer.Trigger className="absolute m-4 right-0 top-0 h-9 w-9 touch-none border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0">
-        <Filter />
-      </Drawer.Trigger>
+      <Button
+        className="absolute m-4 right-0 top-0 h-9 w-9 touch-none"
+        variant="outline"
+        asChild
+      >
+        <Drawer.Trigger>
+          <Filter />
+        </Drawer.Trigger>
+      </Button>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
         <Drawer.Content
