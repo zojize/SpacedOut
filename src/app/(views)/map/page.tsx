8fbd2463d1a13f69fcdd3ebcbe480fd2cbfd0a1a
zojize <jizezou2@illinois.edu>
2024-11-11 00:26:56 -0600

    Building markers

--- src/app/(views)/map/page.tsx
+++ src/app/(views)/map/page.tsx
@@ -1,8 +1,14 @@
 'use client';
 
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { Button } from '@/components/ui/button';
+import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
+import { User } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { features as buildings } from '@/data/uiuc_buildings.json';
 
 export default function Page() {
+  const router = useRouter();
+
   return (
     <>
       <APIProvider
@@ -18,7 +24,40 @@ export default function Page() {
           gestureHandling={'greedy'}
           disableDefaultUI
           mapId="49ae42fed52588c3"
-        />
+        >
+          {buildings.map(({ geometry, properties }, i) => (
+            <AdvancedMarker
+              key={i}
+              position={{
+                lng: geometry.coordinates[0],
+                lat: geometry.coordinates[1],
+              }}
+              title={properties.name}
+              onClick={() => router.push(`/todo}`)}
+            >
+              <Button
+                className="rounded-full bg-green-300"
+                size="icon"
+                variant="secondary"
+              >
+                <User />
+              </Button>
+            </AdvancedMarker>
+          ))}
+          {/* <AdvancedMarker
+            position={{ lng: -88.2247336803035, lat: 40.11383687791036 }}
+            title="Marker"
+            onClick={() => console.log('clicked')}
+          >
+            <Button
+              className="rounded-full bg-green-300"
+              size="icon"
+              variant="secondary"
+            >
+              <User />
+            </Button>
+          </AdvancedMarker> */}
+        </Map>
       </APIProvider>
     </>
   );
