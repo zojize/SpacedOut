8cbe06cc64d8b26d34f69053e0b3446341f848b6
tuonganhdo <anhdotuong@gmail.com>
2024-11-14 20:54:37 -0600

    Added list filtering

--- src/app/(views)/list/page.tsx
+++ src/app/(views)/list/page.tsx
@@ -2,18 +2,25 @@
 
 import { useRouter } from 'next/navigation';
 import { BuildingCard } from '@/components/ui/building_card';
+import { useFilters } from '@/hooks/useFilters';
 import buildings from '@/data/tagging.json';
 
 export default function Page() {
   const router = useRouter();
-
+  const [filters] = useFilters();
+  
   return (
     <div className="w-screen h-screen">
-      {buildings.map((building, i) => (
-        <BuildingCard 
-          key={i}
-          building={building}/>
-      ))}
+      {buildings.map((building, i) => {
+        const isFilteredOut = filters.size > 0 && Array.from(filters).some((filter) => !building?.[filter]);
+        if (!isFilteredOut) {
+          return(
+            <BuildingCard 
+              key={i}
+              building={building}/>
+          );
+        }
+      })}
       <div className="h-1 w-screen"/>
     </div>
   );
--- src/hooks/useFilters.ts
+++ src/hooks/useFilters.ts
@@ -4,7 +4,7 @@ export type FilterTypes = 'quiet'
                         | 'talkative'
                         | 'open_late'
                         | 'coffee_shop'
-                        | 'vending machine'
+                        | 'vending_machine'
                         | 'big_tables'
                         | 'couches';
 
@@ -12,4 +12,4 @@ export const useFilters = () =>
    useLocalStorage('filters', new Set<FilterTypes>, {
     serializer: filters => JSON.stringify(Array.from(filters)),
     deserializer: filters => new Set(JSON.parse(filters)), 
-  })
+  })          
