6f504b8673e7a6c83842a7148c2581e4054f7f23
zojize <jizezou2@illinois.edu>
2024-11-15 01:12:55 -0600

    Remove deprecated props

--- src/app/buildinginfo/[buildingName]/page.tsx
+++ src/app/buildinginfo/[buildingName]/page.tsx
@@ -14,27 +14,11 @@ export default function Page() {
       buildingName={decodeURIComponent(buildingName) as keyof typeof buildings} //placeholder
       buildingAddress="1405 W Springfield Ave, Urbana, IL 61801" //placeholder
       //hours="24 hours"
-      hours={{
-        'M-TH': '7AM-11PM',
-        F: '7AM-9PM',
-        SAT: '9AM-9PM',
-        SUN: '12PM-11PM',
-      }}
       // either specify a start and end date, or just a single date
       selectedTime={
         //{{start: new Date('November 12, 2024 09:51:00'), end: new Date('November 12, 2024 09:59:00')}}
         new Date()
       }
-      tags={{
-        quiet: 0,
-        talkative: 1,
-        open_late: 1,
-        coffee_shop: 1,
-        big_tables: 1,
-        couches: 0,
-        vending_machine: 1,
-        crowd_level: 1,
-      }}
     />
   );
 }
